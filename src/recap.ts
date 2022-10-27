6d0d95e20984db7ada22914b85aa78f1eada65ea
Raimundo Ladrón de Guevara <raimundo.ladrondeguevara@applydigital.com>
2022-10-27 19:03:24 -0300

    feat: create application

---
+++ src/recap.ts
@@ -0,0 +1,23 @@
+// const myName = 'Raimundo';
+// const myAge = 12;
+
+// const suma = (a: number, b: number) => {
+//   return a + b;
+// };
+
+// suma(5, 7);
+
+// class Person {
+//   constructor(private age: number, private name: string) {
+//     this.age = age;
+//     this.name = name;
+//   }
+
+//   getSummary() {
+//     return `My name is ${this.name}, I am ${this.age} years old.`;
+//   }
+// }
+
+// const Raimundo = new Person(31, 'Raimundo');
+// Raimundo.getSummary();
+// console.log();
